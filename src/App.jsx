809f6215baada6517817848f9f91b068ac4a2dff
stephen <soyekale1@gmail.com>
2026-06-24 23:01:19 +0100

    Created homepage navigation and hero section

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,27 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <nav>
+      <nav className="navbar">
         <h2>Enckaf Digital</h2>
+
+        <ul>
+          <li>Home</li>
+          <li>AWS Cloud</li>
+          <li>AI Agents</li>
+          <li>Training</li>
+          <li>Contact</li>
+        </ul>
       </nav>
 
       <section className="hero">
         <h1>AWS Cloud & AI Solutions</h1>
 
         <p>
-          Helping businesses adopt AWS Cloud, AI Agents,
-          Automation, and Digital Transformation.
+          Helping businesses adopt AWS Cloud, AI Automation,
+          AI Agents and Digital Transformation.
         </p>
 
-        <button>Get Consultation</button>
+        <button>Book Consultation</button>
       </section>
     </div>
   );
