a2fbe92634ad9ff418390ec649cb169a1eca1fbc
stephen <soyekale1@gmail.com>
2026-06-24 22:31:29 +0100

    Initial Enckaf Digital website

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <h2>Enckaf Digital</h2>
+      </nav>
+
+      <section className="hero">
+        <h1>AWS Cloud & AI Solutions</h1>
+
+        <p>
+          Helping businesses adopt AWS Cloud, AI Agents,
+          Automation, and Digital Transformation.
+        </p>
+
+        <button>Get Consultation</button>
+      </section>
+    </div>
+  );
+}
+
+export default App;
